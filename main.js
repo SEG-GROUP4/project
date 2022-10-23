0ea396b54bb66d881b86cfec07447c85283dda3c
uoRetr0 <114036712+uoRetr0@users.noreply.github.com>
2022-10-22 20:49:13 -0400

    Back to top button + interactive navbar

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-var a = 1;
+// take user back up
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
+
+
+// show active menu when scrolling
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+  const offset = window.pageYOffset;
+  
+  if(offset > 150)
+    nav.classList.add('scroll')
+  else 
+    nav.classList.remove('scroll')
+});
