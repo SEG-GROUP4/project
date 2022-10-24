70e2d11952881981eff42d98a2be30e65dd90fc7
uoRetr0 <114036712+uoRetr0@users.noreply.github.com>
2022-10-23 23:27:47 -0400

    confirmation step to sign up button

--- main.js
+++ main.js
@@ -13,3 +13,96 @@ window.addEventListener('scroll', function() {
   else 
     nav.classList.remove('scroll')
 });
+
+
+// make button disapear after click
+const disappear = () => {
+  var slideSource = document.getElementById('button');
+  document.getElementById("button").disabled = true; 
+  document.getElementById('button')
+    slideSource.classList.add('fade');
+
+ var slideSource = document.getElementById('status');
+ document.getElementById('status')
+   slideSource.classList.add('fade');
+ 
+} 
+
+// slide to confirm sign up
+$("#confirm").on('change',function() {
+  var slidepos = $(this).val();
+  if(slidepos > 99){
+   	// User slided the slider
+     var slideSource = document.getElementById('confirm');
+     document.getElementById('confirm')
+       slideSource.classList.add('fade');
+
+    var slideSource = document.getElementById('.delete-notice');
+    document.getElementById('.delete-notice')
+      slideSource.classList.add('fade');
+    
+
+  } else {
+    $(this).val(0);
+  }
+});
+
+// rep
+ const disappear2 = () => {
+  var slideSource = document.getElementById('button2');
+  document.getElementById("button2").disabled = true; 
+  document.getElementById('button2')
+    slideSource.classList.add('fade');
+
+ var slideSource = document.getElementById('status2');
+ document.getElementById('status2')
+   slideSource.classList.add('fade');
+ 
+}
+
+$("#confirm2").on('change',function() {
+  var slidepos = $(this).val();
+  if(slidepos > 99){
+     var slideSource = document.getElementById('confirm2');
+     document.getElementById('confirm2')
+       slideSource.classList.add('fade');
+
+    var slideSource = document.getElementById('.delete-notice2');
+    document.getElementById('.delete-notice2')
+      slideSource.classList.add('fade');
+    
+
+  } else {
+    $(this).val(0);
+  }
+});
+
+//rep
+const disappear3 = () => {
+  var slideSource = document.getElementById('button3');
+  document.getElementById("button3").disabled = true; 
+  document.getElementById('button3')
+    slideSource.classList.add('fade');
+
+ var slideSource = document.getElementById('status3');
+ document.getElementById('status3')
+   slideSource.classList.add('fade');
+ 
+} 
+
+$("#confirm3").on('change',function() {
+  var slidepos = $(this).val();
+  if(slidepos > 99){
+     var slideSource = document.getElementById('confirm3');
+     document.getElementById('confirm3')
+       slideSource.classList.add('fade');
+
+    var slideSource = document.getElementById('.delete-notice3');
+    document.getElementById('.delete-notice3')
+      slideSource.classList.add('fade');
+    
+
+  } else {
+    $(this).val(0);
+  }
+});
