a4897eef473dc1c1328e9664f6b94b71567d9357
Mohamed7327 <114036676+Mohamed7327@users.noreply.github.com>
2022-10-26 14:47:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
 // show active menu when scrolling
 const nav = document.querySelector('nav');
 
-//window.addEventListener('scroll', function() { (MOHAMED)//
+window.addEventListener('scroll', function() {
   const offset = window.pageYOffset;
   
   if(offset > 150) 
